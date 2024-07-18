9df0ca5a24eda651ffd22aa0137b6c76e523dd12
Tomas Rawski <rtomas@gmail.com>
2024-07-18 10:57:42 -0300

    featureadwallets

--- src/main.tsx
+++ src/main.tsx
@@ -39,6 +39,10 @@ createWeb3Modal({
   solanaConfig,
   chains,
   projectId,
+  featuredWalletIds: [
+    'c57ca95b47569778a828d19178114f4db188b89b763c899ba0be274e97267d96', // metamask
+    'c03dfee351b6fcc421b4494ea33b9d4b92a984f87aa76d1663bb28705e95034a'  // uniswap wallet
+]
   wallets: [  
     /* new PhantomWalletAdapter(),
     new HuobiWalletAdapter(),
